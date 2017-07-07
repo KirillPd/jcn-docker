4ea690b156af3b4481069f2b22228fcf4381c7ec
kpoddubnyi <kpoddubnyi@justcoded.co>
2017-07-07 13:43:19 +0300

    Remove docker's files

--- wsk.sh
+++ wsk.sh
@@ -8,8 +8,10 @@ then
     mkdir container
     #Run docker container
     docker run -dit -v $(pwd)/container:/var/tmp --name wsk justcoded/wsk
-    #Execute jcn inside the container. Move files to tmp folder and install node modules
-    docker exec -it wsk sh -c "./dockerrun.sh && mv * ../tmp && cd ../tmp && npm install"
+    #Execute jcn inside the container. Move files to tmp folder, delete Docker's files and install node modules
+    docker exec -it wsk sh -c "./dockerrun.sh && mv * ../tmp && cd ../tmp && find . -iname \"[Dd]ocker*\" -maxdepth 1 -delete && npm install && exit"
+    #Stop and remove docker container
+    docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
 elif [ "$1" == "start" ];
 then
     TEST_CONTAINER=$(docker run -p 8080:8080 --name wsk -dit -v $(pwd)/container:/var/app justcoded/wsk) && docker logs $TEST_CONTAINER -f
