b3902f2ed28507c585dafccac2014d6a5ddc7500
kpoddubnyi <kpoddubnyi@justcoded.co>
2017-07-07 16:38:34 +0300

    Add production and develop commands

--- wsk.sh
+++ wsk.sh
@@ -2,7 +2,7 @@
 
 #TODO: if [ "$1" == "install" ];
 
-if [ "$1" == "projectstart" ];
+if [ "$1" == "projectstart" ]
 then
     #Create the container folder
     mkdir container
@@ -12,7 +12,17 @@ then
     docker exec -it $CONTAINER sh -c "./dockerrun.sh && mv * ../tmp && cd ../tmp && find . -maxdepth 1 -iname \"[Dd]ocker*\" -delete && npm install && exit"
     #Stop and remove docker container
     docker stop $CONTAINER && docker rm $CONTAINER
-elif [ "$1" == "start" ];
+elif [ "$1" == "serve" ]
 then
-    CONTAINER=$(docker run -p 8080:8080 -dit -v $(pwd)/container:/var/app justcoded/wsk npm start) && docker logs $CONTAINER -f
+    CONTAINER=$(docker run -dit -p 8080:8080 -v $(pwd)/container:/var/app justcoded/wsk npm start) && docker logs $CONTAINER -f
+elif [ "$1" == "production" ]
+then
+    CONTAINER=$(docker run -dit -v $(pwd)/container:/var/app justcoded/wsk npm run production) && docker logs $CONTAINER -f
+    #Stop and remove docker container
+    docker stop $CONTAINER && docker rm $CONTAINER
+elif [ "$1" == "develop" ]
+then
+    CONTAINER=$(docker run -dit -v $(pwd)/container:/var/app justcoded/wsk npm run develop) && docker logs $CONTAINER -f
+    #Stop and remove docker container
+    docker stop $CONTAINER && docker rm $CONTAINER
 fi
