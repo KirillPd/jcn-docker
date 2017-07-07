b8235f9787397e7bb9cef5569fb96a47a6ba57bf
kpoddubnyi <kpoddubnyi@justcoded.co>
2017-07-07 16:16:52 +0300

    Create a CONTAINER variable

--- wsk.sh
+++ wsk.sh
@@ -7,12 +7,12 @@ then
     #Create the container folder
     mkdir container
     #Run docker container
-    docker run -dit -v $(pwd)/container:/var/tmp --name wsk justcoded/wsk
+    CONTAINER=$(docker run -dit -v $(pwd)/container:/var/tmp --name wsk justcoded/wsk)
     #Execute jcn inside the container. Move files to tmp folder, delete Docker's files and install node modules
-    docker exec -it wsk sh -c "./dockerrun.sh && mv * ../tmp && cd ../tmp && find . -iname \"[Dd]ocker*\" -maxdepth 1 -delete && npm install && exit"
+    docker exec -it $CONTAINER sh -c "./dockerrun.sh && mv * ../tmp && cd ../tmp && find . -maxdepth 1 -iname \"[Dd]ocker*\" -delete && npm install && exit"
     #Stop and remove docker container
-    docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
+    docker stop $CONTAINER && docker rm $CONTAINER
 elif [ "$1" == "start" ];
 then
-    TEST_CONTAINER=$(docker run -p 8080:8080 --name wsk -dit -v $(pwd)/container:/var/app justcoded/wsk) && docker logs $TEST_CONTAINER -f
+    CONTAINER=$(docker run -p 8080:8080 -dit -v $(pwd)/container:/var/app justcoded/wsk npm start) && docker logs $CONTAINER -f
 fi
