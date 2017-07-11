936660e958619867f49e62ca412a729df3cd90bf
kpoddubnyi <kpoddubnyi@justcoded.co>
2017-07-11 15:26:11 +0300

    Refactoring

--- wsk.sh
+++ wsk.sh
@@ -11,16 +11,13 @@ if [ "$1" == "project-start" ]; then
     docker exec -it $CONTAINER sh -c "cd /var/tmp && npm install jpegtran-bin --no-bin-links && npm rebuild node-sass --force --no-bin-links && exit"
     #Stop and remove docker container
     docker kill $CONTAINER && docker rm $CONTAINER
-elif [ "$1" == "serve" ]; 
-then
+elif [ "$1" == "serve" ]; then
     CONTAINER=$(docker run -dit $CONTAINER --expose 8080 -p 8080:8080 -v $(pwd)/container:/var/app 39281706/wsk npm start) && docker logs $CONTAINER -f
-elif [ "$1" == "production" ]; 
-then
+elif [ "$1" == "production" ]; then
     CONTAINER=$(docker run -dit -v $(pwd)/container:/var/app 39281706/wsk npm run production) && docker logs $CONTAINER -f
     #Stop and remove docker container
     docker stop $CONTAINER && docker rm $CONTAINER
-elif [ "$1" == "develop" ]; 
-then
+elif [ "$1" == "develop" ]; then
     CONTAINER=$(docker run -dit -v $(pwd)/container:/var/app 39281706/wsk npm run develop) && docker logs $CONTAINER -f
     #Stop and remove docker container
     docker stop $CONTAINER && docker rm $CONTAINER
